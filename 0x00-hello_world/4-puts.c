715e6a7d152c3b9ba0296ed5b52d63a8546ffcaf
daveedx <david.aki@stu.cu.edu.ng>
2022-09-08 23:41:08 +0000

    k

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/**
+ * main - Main Function
+ *
+ * Description: Code that prints a given message on the screen
+ *
+ * Return: Returns Zero
+ */
+
+int main(void)
+{
+	char message = "Programming is like building a multilingual puzzle";
+
+	puts(message);
+
+	return (0);
+}
