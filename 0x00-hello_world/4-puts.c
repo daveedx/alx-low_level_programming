4871d84ca5987c9eaf8db0334f2305e5151615b0
daveedx <david.aki@stu.cu.edu.ng>
2022-09-08 23:42:33 +0000

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char message = "Programming is like building a multilingual puzzle";
+	char message[] = "Programming is like building a multilingual puzzle";
 
 	puts(message);
 
