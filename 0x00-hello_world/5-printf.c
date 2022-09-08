5ba7d2a0239ddb99ef9a2373732d299048354590
daveedx <david.aki@stu.cu.edu.ng>
2022-09-08 23:52:59 +0000

    k

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Prints out a message using the printf function
+ *
+ * Return: Returns 0 always
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
