395c6344868ae644c3c796868a8e7c65efe2009a
daveedx <david.aki@stu.cu.edu.ng>
2022-09-09 21:32:48 +0000

    k

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Gets the last number of a random digit and Checks it
+ *
+ * Return: Returns Zero
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		if (c == 'e' || c == 'q')
+			return ();
+		puchar(c);
+	}
+
+	return (0);
+}
