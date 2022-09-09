3d4f94b412e052958212f9015f01b5a99fc599cf
daveedx <david.aki@stu.cu.edu.ng>
2022-09-09 21:43:30 +0000

    l

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Gets the last number of a random digit and Checks it
+ *
+ * Return: Returns Zero
+ */
+int main(void)
+{
+	int digit;
+	
+	for (digit = 0; digit < 10; ++digit)
+	{
+		printf("%d", digit);
+	}
+
+	return (0);
+}
