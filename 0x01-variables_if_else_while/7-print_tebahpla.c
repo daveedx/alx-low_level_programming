23fe95de25e9b16576c2d8b0296b687c061f6fbd
daveedx <david.aki@stu.cu.edu.ng>
2022-09-09 21:58:43 +0000

    k

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Gets the last number of a random digit and Checks it
+ *
+ * Return: Returns Zero
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'a'; --c)
+		putchar(c);
+
+	return (0);
+}
