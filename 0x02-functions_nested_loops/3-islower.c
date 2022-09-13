670836337fbb0f48df8beb9f86d87f3ad8b4fcff
daveedx <david.aki@stu.cu.edu.ng>
2022-09-13 22:03:35 +0000

    k

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 /**
  * _islower - Checks if a char is lower
- * @ c: the character to be checked
+ * @c: checks input of function
  *
  * Description: A C progrma that prints using _putchar
  * Return: Program should return 0
