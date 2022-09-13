ffd2aab8714debf83b80ffe62fba322d2f0a7e7e
daveedx <david.aki@stu.cu.edu.ng>
2022-09-13 21:37:32 +0000

    l

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints a-z 10 times
+ *
+ * Description: A C progrma that prints using _putchar
+ * Return: Program should return 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char f;
+
+	for (int x = 0; x <= 10; ++x)
+	{
+		for (f = 'a'; f <= 'z'; ++f)
+		{
+			_putchar(f);
+		}
+	}
+
+}
