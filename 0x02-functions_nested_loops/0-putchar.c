db9647e6b1fa26c21b1ca0119fa7825834f7a263
daveedx <david.aki@stu.cu.edu.ng>
2022-09-13 21:15:49 +0000

    k

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "_putchar.c"
+/**
+ * main - Entry point
+ *
+ * Description: A C progrma that prints using _putchar
+ * Return: Program should return 0
+ */
+
+int main(void)
+{
+	char strng[] = "_putchar";
+	int char;
+
+	for (chr = 0; chr <= 7; ++chr)
+	{
+		_putchar(strng[chr]);
+	}
+	_putchar('\n');
+	return (0);
+}
